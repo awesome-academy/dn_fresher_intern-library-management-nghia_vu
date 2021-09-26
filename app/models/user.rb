47c3c3e37db11e2e1eca778dc141cb215922fd07
vunn-2690 <nguyen.nam.vu@sun-asterisk.com>
2021-09-26 14:41:08 +0700

    Sign Up
    fix i18n for forgot password
    
    add i18n for fail reg
    
    fix comment
    
    fix space

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   enum status: {member: 0, admin: 1}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :name, presence: true, length: {minimum: Settings.length.digit_10}
+  validates :email, presence: true, uniqueness: true,
+    length: {maximum: Settings.length.digit_255},
+    format: {with: VALID_EMAIL_REGEX}
+  validates :password, length: {minimum: Settings.length.digit_8}
   has_secure_password
 
   def self.digest string
